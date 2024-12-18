7a2b2ffeb46c9a93d28b0e4ce1b96def2f295f36
John Gaitho <jgaitho016@gmail.com>
2024-12-18 21:56:48 +0300

    get,del & find Album

--- orm.py
+++ orm.py
@@ -29,3 +29,31 @@ def find_artist_by_id(artist_id):
     artist = session.query(Artist).get(artist_id)
     session.close()
     return artist
+
+def create_album(title, artist_id):
+    Session = Session()
+    artist = Session.query(Artist).get(artist_id)
+    if artist:
+        new_album = Album(title=title, artist_id=artist.id)
+        Session.add(new_album)
+        Session.commit()
+        Session.close()
+    else:
+        print(f"Artist with ID {artist_id} not found.")
+
+def delete_albulm(album_id):
+    Session = Session() 
+    album = Session.query(Album).get(album_id)
+    if album:
+        Session.delete(album)
+        Session.commit()
+        Session.close()
+    else:
+        print(f"Album with ID {album_id} not found.")
+
+def find_albulm_by_id(album_id):
+    Session = Session() 
+    album = Session.query(Album).get(album_id)
+    Session.close() 
+    return album
+      
