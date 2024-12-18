b29295e8a84a13eb117b6a0eb3316f34d1963224
John Gaitho <jgaitho016@gmail.com>
2024-12-18 20:27:25 +0300

    tables

--- models.py
+++ models.py
@@ -1,16 +1,16 @@
 from sqlalchemy import *
 from sqlalchemy.orm import *
 from sqlalchemy.ext.declarative import *
-
 Base = declarative_base()
 
 
-class Artist(base):
+class Artist(Base):
     __tablename__ = 'artists'
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)
-    albums = relationship('Album', back_populates='artist', cascade='all, delete-orphan')
+    id = Column(Integer, primary_key=True)    # to define a column id which is an integer.
+    name = Column(String, nullable=False)     # for name which cannot be null.
+
+    albums = relationship('Album', back_populates='artist', cascade='all, delete-orphan') # to delete all related albs when artist is deleted.
 
     def __repr__(self):
         return f"<Artist(name={self.name})>"
@@ -22,12 +22,20 @@ class Album(Base):
     title = Column(String, nullable=False)
     artist_id = Column(Integer, ForeignKey('artists.id'))
 
-    artist = relationship('Artist', back_populates='albums') #  argument for relationship with the 
+    artist = relationship('Artist', back_populates='albums')                             # a two way relashionship btn album & artist.
+    tracks = relationship('Track', back_populates='album', cascade='all, delete-orphan')
+
+    def __repr__(self):
+        return f"<Album(title={self.title}, artist={self.artist.name})>"
 
 class Track(Base):
     __tablename__ = 'tracks'
 
     id = Column(Integer, primary_key=True)
     title = Column(String, nullable=False)
-    album_id = Column(Integer, ForeignKey('albums.id'))
+    album_id = Column(Integer, ForeignKey('albums.id'))  #to link each track to an album. 
 
+    album = relationship('Album', back_populates='tracks') 
+
+    def __repr__(self):
+        return f"<Track(title={self.title}, album={self.album.title})>"
