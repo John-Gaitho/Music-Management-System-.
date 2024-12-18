92743bebd34644ffc7a7282389908dd10bf0de34
John Gaitho <jgaitho016@gmail.com>
2024-12-18 20:02:50 +0300

    albulm table

--- models.py
+++ models.py
@@ -24,5 +24,5 @@ class Album(Base):
 
     artist = relationship('Artist', back_populates='albums')
 
-    def __repr__(self):
-        return f"<Album(title={self.title}, artist={self.artist.name})>"
+   
+        
